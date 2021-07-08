32295c86cc29b2df4f8e3498c2d5387c652158e1
Vaibhav Gope <vaibhavgope02@gmail.com>
2021-07-08 10:34:20 +0530

    UI improvements

--- crowdfunding-page/src/App.js
+++ crowdfunding-page/src/App.js
@@ -5,19 +5,25 @@ import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 import { cyan } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
-  palette: {
-    primary: { main: cyan[500] },
-    secondary: { main: cyan[700] },
-  },
+    typography: {
+        allVariants: {
+            color: 'hsl(200, 15%, 8%)'
+        },
+        fontFamily: 'Roboto'
+    },
+    palette: {
+        primary: { main: cyan[500] },
+        secondary: { main: cyan[700] },
+    },
 });
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <LandingPage />
-      </div>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <LandingPage />
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
